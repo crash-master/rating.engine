1db2575cc803427feb4559bae40000fc7088009b
s_eugene_r <s_eugene_r@yahoo.com>
2018-10-20 22:00:43 +0300

    new com and fix com

--- fw/modules/com/com-routes.php
+++ fw/modules/com/com-routes.php
@@ -1,17 +1,17 @@
 <?php 
 // pages
-route('/com', 'Modules\\comController@dashboard');
-route('/com/about', 'Modules\\comController@about');
+route('/com', 'Modules\\ComController@dashboard');
+route('/com/about', 'Modules\\ComController@about');
 
 // api
-route('/com/create/controller/{name}', 'Modules\\comController@createController');
-route('/com/create/model/{name}', 'Modules\\comController@createModel');
-route('/com/create/set/{name}', 'Modules\\comController@createSet');
-route('/com/create/migration/{name}', 'Modules\\comController@createMigration');
-route('/com/migrations/up/{name}', 'Modules\\comController@migrationUp');
-route('/com/migrations/down/{name}', 'Modules\\comController@migrationDown');
-route('/com/migrations/up', 'Modules\\comController@migrationUpAll');
-route('/com/migrations/down', 'Modules\\comController@migrationDownAll');
-route('/com/migrations/list', 'Modules\\comController@migrationList');
-route('/com/migrations/refresh/{name}', 'Modules\\comController@migrationRefresh');
-route('/com/migrations/refresh', 'Modules\\comController@migrationRefreshAll');
+route('/com/create/controller/{name}', 'Modules\\ComController@createController');
+route('/com/create/model/{name}', 'Modules\\ComController@createModel');
+route('/com/create/set/{name}', 'Modules\\ComController@createSet');
+route('/com/create/migration/{name}', 'Modules\\ComController@createMigration');
+route('/com/migrations/up/{name}', 'Modules\\ComController@migrationUp');
+route('/com/migrations/down/{name}', 'Modules\\ComController@migrationDown');
+route('/com/migrations/up', 'Modules\\ComController@migrationUpAll');
+route('/com/migrations/down', 'Modules\\ComController@migrationDownAll');
+route('/com/migrations/list', 'Modules\\ComController@migrationList');
+route('/com/migrations/refresh/{name}', 'Modules\\ComController@migrationRefresh');
+route('/com/migrations/refresh', 'Modules\\ComController@migrationRefreshAll');
